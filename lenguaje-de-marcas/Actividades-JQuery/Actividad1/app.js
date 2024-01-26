33024f586daa17617750a84d5fc0e08186bdfa28
Juan Antonio Fernandez <121491697+JuanAntonioFernandezMayol@users.noreply.github.com>
2024-01-26 20:31:22 +0100

    rtrtrt

--- lenguaje-de-marcas/Actividades-JQuery/Actividad1/app.js
+++ lenguaje-de-marcas/Actividades-JQuery/Actividad1/app.js
@@ -2,16 +2,17 @@ $(function agregarcomentarios() {
     $("#add").click(function () {
         let newCommentTextBox = $("#comment");
         let commentText = newCommentTextBox.val();
-        $("#comments").append("<li>" + commentText + "</li>");
+        $("#comments").append("<div class='div'><li>" + commentText + "</li><span class='X'>X</span></div>");
         newCommentTextBox.val('');
+            $(".X").click(function(){
+                $(this).parent().remove();
+            })
     });
 });
 
 // + "<button #dele>" + "X" + "</button>" +
 
-function eliminarultimo() {
-    $("li:last").remove();
-}
+
 
 $(function ocultarinfo() {
     $("#comentario").click(function () {
