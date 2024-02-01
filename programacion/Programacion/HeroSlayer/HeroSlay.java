aadecf757a90648ee093f7ad878a51e65d459cfc
Juan Antonio Fernandez <juananfernandez6@gmail.com>
2024-02-01 13:56:24 +0100

    aaa

--- programacion/Programacion/HeroSlayer/HeroSlay.java
+++ programacion/Programacion/HeroSlayer/HeroSlay.java
@@ -1,24 +1,24 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * HeroSlay
  */
 public class HeroSlay {
+    private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         // List<Carta> mazo = new ArrayList<>();
         List<Personaje> personajes = new ArrayList<>();
         Carta Carta1 = new Carta("Pepe", TipoCarta.ATAQUE, 3);
-        Carta1.pintarInformacion();
         Carta Carta2 = new Carta("Luis", TipoCarta.CURA, 2);
-        Carta2.pintarInformacion();
         Carta Carta3 = new Carta("Jose", TipoCarta.ATURDIR, 1);
-        Carta3.pintarInformacion();
+
         Personaje Heroe = new Personaje("Heroe", 10);
         Personaje Villano = new Personaje("Villano", 10);
-        // mazo.add(Carta1);
-        // mazo.add(Carta2);
-        // mazo.add(Carta3);
+
         personajes.add(Heroe);
         personajes.add(Villano);
 
@@ -41,16 +41,61 @@ public class HeroSlay {
     }
 
     private static void jugarRonda(Personaje jugador1, Personaje jugador2) {
-        Carta cartaJugador1 = jugador1.jugarCarta();
+        System.out.println("\n---- Comienza una nueva ronda ----");
+
+        mostrarInformacionJugador(jugador1);
+
+        Carta cartaJugador1 = elegirCarta(jugador1);
+        System.out.println(jugador1.getNombrePersonaje() + " juega la carta: ");
+        cartaJugador1.pintarInformacion();
         jugador2.recibirEfecto(cartaJugador1);
 
-        if (cartaJugador1.getTipoCarta() != TipoCarta.ATURDIR) {
-            Carta cartaJugador2 = jugador2.jugarCarta();
+        if (!jugador2.isAturdido() && cartaJugador1.getTipoCarta() != TipoCarta.ATURDIR) {
+            mostrarInformacionJugador(jugador2);
+
+            Carta cartaJugador2 = elegirCarta(jugador2);
+            System.out.println(jugador2.getNombrePersonaje() + " juega la carta: ");
+            cartaJugador2.pintarInformacion();
             jugador1.recibirEfecto(cartaJugador2);
         }
-        System.out.println("---- Resultado de la ronda ----");
-        System.out.println("Vida del Heroe: " + jugador1.getVidaPersonaje());
-        System.out.println("Vida del Villano: " + jugador2.getVidaPersonaje());
+
+        mostrarResultadoRonda(jugador1, jugador2);
+    }
+
+    private static void mostrarInformacionJugador(Personaje jugador) {
+        System.out.println("\nInformación para " + jugador.getNombrePersonaje() + ":");
+        mostrarCartasAleatorias(jugador);
+    }
+
+    private static void mostrarResultadoRonda(Personaje jugador1, Personaje jugador2) {
+        System.out.println("\n---- Resultado de la ronda ----");
+        System.out.println("Vida del " + jugador1.getNombrePersonaje() + ": " + jugador1.getVidaPersonaje());
+        System.out.println("Vida del " + jugador2.getNombrePersonaje() + ": " + jugador2.getVidaPersonaje());
         System.out.println("--------------");
+    }
+
+    private static void mostrarCartasAleatorias(Personaje jugador) {
+        System.out.println("Cartas en el mazo de " + jugador.getNombrePersonaje() + ":");
+        List<Carta> cartas = jugador.getMazo();
+        Collections.shuffle(cartas);
+
+        for (int i = 0; i < 2 && i < cartas.size(); i++) {
+            System.out.println((i + 1) + ". " + cartas.get(i).getNombreCarta());
+            cartas.get(i).pintarInformacion();
+        }
+    }
+
+    private static Carta elegirCarta(Personaje jugador) {
+        System.out.println("\nElige una carta (1 o 2): ");
+        int opcion;
+        do {
+            System.out.print("-->  ");
+            opcion = scanner.nextInt();
+        } while (opcion < 1 || opcion > 2);
+
+        List<Carta> cartas = jugador.getMazo();
+        Collections.shuffle(cartas); // Barajar las cartas antes de elegir
+
+        return cartas.get(opcion - 1);
     }
 }
--- programacion/Programacion/HeroSlayer/Personaje.java
+++ programacion/Programacion/HeroSlayer/Personaje.java
@@ -3,14 +3,16 @@ import java.util.List;
 import java.util.Random;
 
 public class Personaje {
-    public String nombrePersonaje;
-    public Integer vidaPersonaje;
-    public List<Carta> mazo;
+    private String nombrePersonaje;
+    private Integer vidaPersonaje;
+    private List<Carta> mazo;
+    private boolean aturdido;
 
     public Personaje(String nombrePersonaje, Integer vidaPersonaje) {
         this.nombrePersonaje = nombrePersonaje;
         this.vidaPersonaje = vidaPersonaje;
         this.mazo = new ArrayList<>();
+        this.aturdido = false;
     }
 
     public void mostrarInformacion() {
@@ -30,13 +32,31 @@ public class Personaje {
 
     public void recibirEfecto(Carta carta) {
         if (carta != null) {
-            if (carta.getTipoCarta() == TipoCarta.ATAQUE) {
-                vidaPersonaje -= carta.getAtaqueCarta();
-                if (vidaPersonaje < 0)
-                    vidaPersonaje = 0;
-            } else if (carta.getTipoCarta() == TipoCarta.CURA) {
-                vidaPersonaje += carta.getAtaqueCarta();
+            if (!aturdido) {
+                if (carta.getTipoCarta() == TipoCarta.ATAQUE) {
+                    vidaPersonaje -= carta.getAtaqueCarta();
+                    if (vidaPersonaje < 0)
+                        vidaPersonaje = 0;
+                } else if (carta.getTipoCarta() == TipoCarta.CURA) {
+                    vidaPersonaje += carta.getAtaqueCarta();
+                }
+                if (carta.getTipoCarta() == TipoCarta.ATURDIR) {
+                    aturdido = true;
+                }
+            } else {
+                System.out.println(nombrePersonaje + " está aturdido y no puede jugar cartas en esta ronda.");
+                aturdido = false; // Desactivamos el aturdimiento para la próxima ronda
             }
+        }
+    }
+
+    public Carta elegirCartaAleatoria() {
+        Random random = new Random();
+        if (!mazo.isEmpty()) {
+            return mazo.get(random.nextInt(mazo.size()));
+        } else {
+            System.out.println(nombrePersonaje + " no tiene cartas en el mazo.");
+            return null;
         }
     }
 
@@ -50,5 +70,17 @@ public class Personaje {
 
     public void setVidaPersonaje(Integer vidaPersonaje) {
         this.vidaPersonaje = vidaPersonaje;
+    }
+
+    public List<Carta> getMazo() {
+        return mazo;
+    }
+
+    public String getNombrePersonaje() {
+        return nombrePersonaje;
+    }
+
+    public boolean isAturdido() {
+        return aturdido;
     }
 }
