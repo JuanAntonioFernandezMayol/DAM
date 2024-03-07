bd07cae76ce5284fd91a14c58081df7bb6c537d8
Juan Antonio Fernandez <121491697+JuanAntonioFernandezMayol@users.noreply.github.com>
2024-03-07 20:11:38 +0100

    aa

---
+++ programacion/Programacion/LecturaDeProductos/Producto.java
@@ -0,0 +1,47 @@
+package Programacion.LecturaDeProductos;
+
+public class Producto {
+    private Integer codigo;
+    private String nombre;
+    private Double precio;
+
+    public Producto(Integer codigo, String nombre, Double precio) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+
+    public void mostrarproducto() {
+        System.out.println(nombre);
+        System.out.println(precio);
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Double precio) {
+        this.precio = precio;
+    }
+}
---
+++ programacion/Programacion/LecturaDeProductos/ProductosApp.java
@@ -0,0 +1,43 @@
+package Programacion.LecturaDeProductos;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class ProductosApp {
+    public static void main(String[] args) {
+        HashMap<Integer, Producto> inventario = new HashMap<>();
+
+        try {
+            String path = "C:\\Users\\alumne-DAM\\Documents\\GitHub\\DAM\\programacion\\Programacion\\LecturaDeProductos\\texto.txt";
+            BufferedReader br = new BufferedReader(new FileReader(path));
+            String text = br.readLine();
+
+            while (text != null) {
+                String[] linia = text.split(",");
+                Producto producto = new Producto(Integer.valueOf(linia[0]), linia[1], Double.parseDouble(linia[2]));
+                text = br.readLine();
+                inventario.put(Integer.valueOf(linia[0]), producto);
+                // inventario.put(Integer.valueOf(linia[3]), producto);
+                // inventario.put(Integer.valueOf(linia[6]), producto);
+            }
+        } catch (IOException e) {
+            System.out.println("No a funcionado");
+
+        }
+        Integer codigo = -1;
+        while (codigo == 0) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Introducce un codigo de barras: ");
+            codigo = sc.nextInt();
+            inventario.get(codigo);
+            if (inventario.get(codigo) != null) {
+                System.out.println("El producto es: " + inventario.(producto.getCodigo()).getNombre() + producto.getNombre);
+            } else {
+                System.out.println("El producto no a sido encontrado!");
+            }
+        }
+    }
+}
