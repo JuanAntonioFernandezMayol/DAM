f00f8c2b84eb5d661642e481ff86b92d8677e10e
Juan Antonio Fernandez <121491697+JuanAntonioFernandezMayol@users.noreply.github.com>
2024-03-13 15:16:25 +0100

    Actividad React

---
+++ lenguaje-de-marcas/raza-gatos/src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Breeds from './views/Breeds';
+import Inicio from './views/home';
+
+function App() {
+  return (
+    <div>
+    <BrowserRouter>
+    <div>
+      <Navbar bg="dark" data-bs-theme="dark" className='menu'>
+        <Container>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+          <Nav>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/breeds">Listado de razas</Nav.Link>
+            {/* <Nav.Link as={Link} to="/detallesDeRazas">Detalles de las razas</Nav.Link> */}
+          </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+
+      
+        <Routes>
+          <Route path="/breeds" element={<Breeds />} />
+          <Route path="/" element={<Inicio />} />
+        </Routes>
+    </div>
+    </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ lenguaje-de-marcas/raza-gatos/src/views/home.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+function Inicio() {
+    return (
+    <div className="text-center p-3 mb-2 bg-dark text-white  height: '100px'}">
+    <h1>Buscador de razas de gatos</h1>
+    <p>Mallorca, la mayor de las Islas Baleares en el Mar Mediterráneo, es conocida por sus impresionantes playas, su clima cálido y su rica historia y cultura. Desde los majestuosos paisajes montañosos hasta las encantadoras ciudades costeras, Mallorca ofrece una amplia gama de experiencias para los visitantes.</p>
+    <p>Explora las estrechas calles empedradas de Palma, la capital de la isla, donde encontrarás impresionantes edificios históricos como la catedral gótica de La Seu y el castillo de Bellver. Disfruta de las hermosas playas de arena blanca y aguas cristalinas en lugares como Cala d'Or, Cala Millor y Playa de Muro.</p>
+    <p>Además de su belleza natural, Mallorca ofrece una deliciosa gastronomía, que incluye platos tradicionales como el tumbet, el frito mallorquín y la ensaimada, un dulce típico de la isla.</p>
+    <p>¡Bienvenido a Mallorca, donde cada rincón ofrece una nueva aventura!</p>
+    </div>
+    
+    );
+}
+
+export default Inicio;
