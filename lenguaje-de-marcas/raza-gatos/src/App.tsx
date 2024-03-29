ac22936bff3d89e999e1ed213d072c495eb0b797
Juan Antonio Fernandez <juananfernandez6@gmail.com>
2024-03-29 19:08:24 +0100

    update del build5

--- lenguaje-de-marcas/raza-gatos/src/App.tsx
+++ lenguaje-de-marcas/raza-gatos/src/App.tsx
@@ -30,8 +30,8 @@ function App() {
           </Navbar>
 
           <Routes>
-            <Route path="/breeds" element={<Breeds />} />
             <Route path="/" element={<Inicio />} />
+            <Route path="/breeds" element={<Breeds />} />
             <Route path="/breeds/:id" element={<DetallesRaza />} />
 
             <Route index element={<Inicio />} />
